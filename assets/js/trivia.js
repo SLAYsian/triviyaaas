c329fd85efa094a32cca1d1ed96a3ec572fbc0ff
slayonce <katharinemg@gmail.com>
2023-05-01 01:04:51 -0400

    trivia.js added DOM Els

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,15 +1,26 @@
 "use strict";
 
 // SECTION: DOM ELEMENTS
+let questionEl = document.querySelector(".questioni");
+let answerBtnsEl = document.querySelectorAll(".answer");
+let gameScoreEl = document.querySelector(".game-score");
+let modalEl = document.querySelector(".trivia-modal");
+let correctOrIncorrectEl = document.querySelector(".correct-incorrect");
+let modalImg = document.querySelector(".modal-img");
+let coseModalBtn = document.querySelector(".close-modal");
 
 // SECTION: FETCH TRIVIA QUESTIONS
 
 function fetchTriviaQuestions() {
   fetch(`https://opentdb.com/api.php?amount=10&difficulty=easy`).then(function (
     response
-  ) {
+  ) // fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`).then(function (
+  //   response
+  // )
+  {
     if (response.ok) {
       response.json().then(function (data) {
+        // NOTES: function displayTriviaQuestions
         console.log(data);
       });
     } else {
