3495e10f891a50f9526ea146b2768f3d084e01be
slayonce <katharinemg@gmail.com>
2023-05-01 20:59:20 -0400

    added bg img, added functions to display questions, handle correct/incorrect answers

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -2,24 +2,24 @@
 
 // SECTION: DOM ELEMENTS
 let questionEl = document.querySelector(".questioni");
-let answerBtnsEl = document.querySelectorAll(".answer");
+let answerBtnsEl = document.querySelectorAll(".answers");
 let gameScoreEl = document.querySelector(".game-score");
 let modalEl = document.querySelector(".trivia-modal");
 let correctOrIncorrectEl = document.querySelector(".correct-incorrect");
+let correctAnswerEl = document.querySelector(".correct-answer");
 let modalImg = document.querySelector(".modal-img");
-let coseModalBtn = document.querySelector(".close-modal");
+let overlayEl = document.querySelector(".overlay");
+let closeModalBtn = document.querySelector(".close-modal");
 
 // SECTION: FETCH TRIVIA QUESTIONS
 
 function fetchTriviaQuestions() {
   fetch(`https://opentdb.com/api.php?amount=10&difficulty=easy`).then(function (
-    response
-  ) // NOTES: Will use this one once code is written in index // fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`).then(function ( response)
-
-  {
+    response // NOTES: Will use this one once code is written in index // fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`).then(function ( response)
+  ) {
     if (response.ok) {
       response.json().then(function (data) {
-        // NOTES: Will use once written -> function displayTriviaQuestions
+        displayTriviaQuestions(data.results);
         console.log(data);
       });
     } else {
@@ -30,19 +30,62 @@ function fetchTriviaQuestions() {
 
 fetchTriviaQuestions();
 
+// SECTION: FUNCTION Difficulty
+
 // SECTION: FUNCTION Display Trivia Questions
+// FIX: Special characters not displaying
 let currentQuestionIndex = 0;
 let questions = [];
 
-function displayTriviaQuestions(quizData) {}
-function displayQuestion() {}
+function displayTriviaQuestions(triviaData) {
+  questions = triviaData;
+  displayQuestion();
+}
 
-// SECTION: EVENT HANDLER BUTTON
+function displayQuestion() {
+  let question = questions[currentQuestionIndex];
+  questionEl.textContent = question.question;
+
+  let answers = [...question.incorrect_answers, question.correct_answer];
+
+  answerBtnsEl.forEach((button, index) => {
+    button.textContent = answers[index];
+    button.dataset.correct = answers[index] === question.correct_answer;
+  });
+}
+
+// SECTION: EVENT HANDLER BUTTON Correct Answer
+answerBtnsEl.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let correctAnswer = e.target.dataset.correct === "true";
+    handleAnswer(correctAnswer);
+  });
+});
 
 // SECTION: CORRECT/ INCORRECT MODAL
+function handleAnswer(correctAnswer) {
+  let question = questions[currentQuestionIndex];
+  let thisCorrectAnswer = question.correct_answer;
+  if (correctAnswer) {
+    correctOrIncorrectEl.textContent = "CORRECT!";
+    modalImg.src = "";
+    correctAnswerEl.textContent = "";
+  } else {
+    correctOrIncorrectEl.textContent = "INCORRECT!";
+    correctAnswerEl.textContent = `Correct Answer: ${thisCorrectAnswer}`;
+  }
+  modalEl.classList.remove("hidden");
+  overlayEl.classList.remove("hidden");
+}
 
 // SECTION: ADD SCORE
+function addScore() {}
+
+// SECTION: DISPLAY NEXT QUESTION
+function nextQuestion() {}
 
 // SECTION: END OF QUIZ
+function endQuiz() {}
 
 // SECTION: SAVE NAME TO HIGH SCORE/ LOCAL STORAGE
+function saveHighScore(name, score) {}
