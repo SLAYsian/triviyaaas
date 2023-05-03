dfbb792c1d27542d4614e0227e1c21bf1c94af53
Nolan krieger <nolankrieger9@gmail.com>
2023-05-03 11:50:21 -0400

    added javascript to index

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+let nameEl = document.querySelector("#name-form");
+let nameField = document.querySelector("#name-input");
+let description = document.querySelector("#description");
+
+function submitHandler(e){
+    e.preventDefault();
+    if(nameField.value === ""){
+        description.textContent = "you didnt put in a name";
+        return;
+    }
+    localStorage.setItem("current-user", nameField.value);
+    alert("quiz started");
+    window.location.replace("trivia.html")
+}
+nameEl.addEventListener("submit", submitHandler);
+
