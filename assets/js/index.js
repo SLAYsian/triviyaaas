27d164c30be9763b4fc7b02346e2886427a5bd75
Nolan krieger <nolankrieger9@gmail.com>
2023-05-03 19:28:13 -0400

    added javascript elements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,18 @@
 let nameEl = document.querySelector("#name-form");
 let nameField = document.querySelector("#name-input");
 let description = document.querySelector("#description");
-
+let easyBtn = document.getElementById("easy-btn")
+let mediumBtn = document.getElementById("medium-btn")
+let hardBtn = document.getElementById("hard-btn")
+easyBtn.addEventListener("click", ()=>{
+    initTrivia("easy")
+})
+mediumBtn.addEventListener("click", ()=>{
+    initTrivia("medium")
+})
+hardBtn.addEventListener("click", ()=>{
+    initTrivia("hard")
+})
 function submitHandler(e){
     e.preventDefault();
     if(nameField.value === ""){
@@ -9,6 +20,13 @@ function submitHandler(e){
         return;
     }
     localStorage.setItem("current-user", nameField.value);
+    
+}
+function initTrivia(difficulty){
+    let userName = nameField.value;
+    
+    localStorage.setItem("current-user", userName);
+    localStorage.setItem("difficulty", difficulty)
     window.location.replace("trivia.html")
 }
 nameEl.addEventListener("submit", submitHandler);
